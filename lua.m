ef76efe2c2e57b4f32d2179d9c7a17da22a72d0c
C4Cypher <c4cypher@gmail.com>
2014-06-02 10:59:58 -0400

    Update lua.m

--- lua.m
+++ lua.m
@@ -2,27 +2,48 @@
 
 :- interface.
 
-:- import_module list, string, int, float, pair, assoc_list, map, io.
+:- import_module list, string, bool, int, float, pair, assoc_list, map, io.
 
-% Refrence to the lua runtime to be passed for lua operations
-:- type lua.state.
+:- type lua_state.
 
-:- typeclass lua.stack_compatable 
-% For transparently handling lua variables on the stack
+:- type mercury_function == pred(lua_state::in, list(lua_value)::in, list(lua_value)::out) is det.
+:- type c_function.
 
-:- type index --->
-  stack(int);
-  global(string); 
-  environment(string);  
-  registry(string);
-  upvalue(int).  % Manipulating c function upvalues
+:- type lua_refrence.
 
-%:- type userdata. % Not sure how to implement this, do later after I get the rest working
 
+:- type lua_value --->
+    nil;
+    boolean(bool);
+    string(string);
+    string(char);
+    number(int);
+    number(float);
+    table(map(lua_value,lua_value));
+    table(list(lua_value));
+    function(mercury_function);
+    function(c_function);
+    userdata(c_pointer);
+    thread(lua_state);
+    refrence(lua_refrence);
+    invalid.
+    
 
   
 :- implementation.
 
 :- pragma foreign_decl("C", "#include <lua.h>;  #include <lauxlib.h>; #include <lualib.h>;").
+
+:- pragma foreign_type("C", lua_state, "lua_State").
+:- pragma foreign_type("C", c_function, "lua_CFunction").
+
+
+:- type lua_refrence --->
+    stack(int);
+    global(string);
+    registry(string);
+    upvalue(int).
+
+
 
 
