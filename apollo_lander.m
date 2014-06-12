325b530a99465053de2635f31dc16109779556a1
C4Cypher <c4cypher@gmail.com>
2014-06-12 14:28:58 -0400

    An interface that Lua's package.loadlib can find.

---
+++ apollo_lander.m
@@ -0,0 +1,19 @@
+:- module apollo_lander.
+:- interface.
+
+:- import_module lua.
+
+
+
+:- implementation.
+
+:- pragma foreign_import_module("C", lua).
+
+:- pragma foreign_decl("C", "int luaopen_apollo_lander(lua_State *);").
+
+:- pragma foreign_code("C", "int luaopen_apollo_lander(lua_State * L) {
+	if(!apollo_ready(L))
+		return luaM_init_apollo(L);
+	else
+		return 0;
+").
