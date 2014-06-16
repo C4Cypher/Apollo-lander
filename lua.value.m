d32ea027fcec69a2bfad4e375e3ce506d72fb3d4
C4Cypher <c4cypher@gmail.com>
2014-06-16 15:55:23 -0400

    Interface for lua.value

--- lua.value.m
+++ lua.value.m
@@ -2,8 +2,20 @@
 
 :- interface.
 
+:- import_module lua.state.
+
+
 :- typeclass lua_value(T) where [
-    func to_var(T) = lua_var is det,
-    func from_var(lua_var) = T is semidet ].
+	% Push a value onto the stack
+	pred push(lua_state, T, io.state, io.state),
+	mode push(in, in, di, uo) is det,
+    
+	% Get a copy of the value at the supplied index
+	% Fail if wrong type
+	% Leave the stack in the same state it was found
+	func pull(lua_state, int, io.state, io.state) = T,
+	mode pull(in, in, di, uo) = out is semidet ].
+    
+    
     
 :- implementation.
