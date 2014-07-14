17f6cb4df3d2da70c5a0f12c53e18367bb6de643
C4Cypher <c4cypher@gmail.com>
2014-07-14 13:44:27 -0400

    Almost finished with the introduction.

---
+++ apollo.m
@@ -0,0 +1,67 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2014 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: apollo.m.
+% Main author: C4Cypher.
+% Stability: low.
+% 
+% This file presents a simple library interface meant to facilitate the 
+% writing of compiled libraries in the Mercury programming language that can 
+% be easily loaded and used from within the Lua programming language.
+%
+% The main predicate is intended to parse through the source code of a
+% Mercury module in order to produce a Lua script, that when run, returns
+% a Lua module that interfaces with the Mercury module compiled as a dynamic
+% library, and exports the predicates, functions and types in the Mercury
+% module's interface as Lua functions, values and userdata.
+%
+%-----------------------------------------------------------------------------%
+%
+% If you are reading this, you should no doubt be aware that Mercury is a
+% strict language.  This is not merely about having a strict static type
+% or mode system. Mercury was designed with a very specific set of semantics
+% in mind.  
+%
+% Mercury is intended to be a purely declarative language, without 
+% side-effects, and the Melbourne Mercury Compiler was designed specifically 
+% to optimize code written with those strict, declarative semantics in mind.  
+% While the language does allow you to deviate from those semantics, there 
+% is a price to be paid in efficiency and ease-of use.  To do so is generally
+% considered to be a 'bad idea'.
+%
+% In contrast, the Lua programming language is known for flexibility,
+% extendability and for ease of code customization on the fly.  Using syntax
+% influenced by Ada and Eiffel, and borrowing a few functional programming
+% features from Lisp, Lua is simple, lightweight and easy to understand.
+% 
+% One of Lua's most notable features is it's extendability.  There are a
+% multitude of ways to extend Lua's syntax and modify it's behavior,
+% allowing one to define and use Lua with their own semantics, tailored
+% to the programmer's needs.
+%
+% This, coupled with a similarly simple stack based C API, makes Lua a very
+% popular choice for embedding in or binding to foreign languages and
+% environments.
+%
+% For a Mercury programmer, to attempt to embed Lua in Mercury might seem
+% counter-intuitive (if not outright insane).  Trying to work around
+% Lua's dynamic, impure and unpredictable nature would run counter to
+% Mercury's strengths and would be a nightmare to implement.
+%
+% At the same time, the way Lua's C API is implemented, if Lua were to load
+% and invoke exported Mercury predicates and functions from a dynamic library,
+% Mercury would be able to interact with the calling Lua state and it's
+% instantiated variables as immutable values and data structures in a manner
+% preserves Mercury's pure declarative semantics.  Furthermore
+
+
+
+
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
