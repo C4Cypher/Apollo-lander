4b66cb8a8626d498e3bc248b952ab55286499497
C4Cypher <c4cypher@gmail.com>
2014-07-15 18:51:47 -0400

    Another redesign.

--- lua.state.m
+++ lua.state.m
@@ -6,29 +6,19 @@
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
 % 
-% File: lua.m.
+% File: lua.state.m.
 % Main author: C4Cypher.
 % Stability: low.
 % 
-% This file presents a simple library interface meant to facilitate the 
-% writing of compiled libraries in the Mercury programming language that can 
-% be easily loaded and used from within the Lua programming language.
+% This file provides lower level access to the Lua state itself, allowing for
+% stateful interactions with the Lua VM.
 %
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
-:- module lua.
+:- module lua.state.
 
 :- interface.
-
-:- use_module io.
-:- import_module list.
-:- import_module univ.
-
-:- include_module lua.state.
-
-
-:- type io == io.state.
 
 
 
@@ -46,305 +36,54 @@
 :- type lua.state.
 
 	% init(L, !IO).
+	%
 	% Prepares a lua_State for interaction with Mercury
 	%
-:- pred lua.init(lua_state::in, io::di, io::uo) is det.
+:- pred lua.init(state::in, io::di, io::uo) is det.
 
+	% ready(L).
+	% ready(L, IsReady, !IO).
 	%
-	% Verify that Lua is prepared for interaction with Mercury
+	% Verify that Lua is prepared for interaction with Mercury.
 	%
-:- pred lua.ready(lua_state::in) is semidet.
+:- pred lua.ready(state::in) is semidet.
+:- pred lua.ready(state, bool, io, io) is det.
 
 
+	% do(L, something(L, !IO)).
+	%
+	% Perform a stateful action with  Lua.  Will prevent any changes to
+	% values already on the stack, revert any changes already on the stack
+	% and abort if it is unable to do so.
+:- pred lua.do(state, pred(state, io, io)).
+:- mode lua.do(in, (pred(in, di, uo) is det)) is det.
+:- mode lua.do(in, (pred(in, di, uo) is semidet)) is semidet.
+:- mode lua.do(in, (pred(in, di, uo) is multi)) is multi.
+:- mode lua.do(in, (pred(in, di, uo) is nondet)) is nondet.
+
+	% do(L, something(L, !IO)).
+	%
+	% This performs operations on the stack in a stateful manner. Note
+	% that this version of do does not prevent or revert any changes
+	% made to Lua, it only ensures that such operations are performed in
+	% a deterministic manner. 
+:- pred lua.do(state, pred(state, io, io)).
+:- mode lua.do(in, (pred(in, di, uo) is det), di, uo) is det.
+:- mode lua.do(in, (pred(in, di, uo) is semidet), di, uo) is det.
+:- mode lua.do(in, (pred(in, di, uo) is multi), di, uo) is det.
+:- mode lua.do(in, (pred(in, di, uo) is nondet), di, uo) is det.
 
 %-----------------------------------------------------------------------------%
 %
-% Variables
+% The stack
 %
 
-% A Lua variable can be used to store any value that can be stored as a 
-% C type.  Furthermore, because variables are instantiated and stored within
-% the Lua state, Mercury cannot construct, deconstruct, equality test or 
-% refrence Lua variables directly like it can C types. These operations are
-% handled by the C API.
-%
-% Another thing to note is the fact that the lifetimes of Lua variables are
-% manged by the Lua garbage collector, a mark and sweep collector not unlike
-% the Bohem GC. 
-%
-% The current implementation of this library places reservations on and
-% registers finalizer callbacks with variables passed by refrence between
-% Mercury and Lua in such a manner that a Variable passed to it's non-native 
-% environment will not be collected by it's own garbage collector until the
-% non-native garbage collector calls the finalizer associated with it.
-
- 
-	% The var type represents an indirect refrence to a variable 
-	% instantiated in Lua. So long as Mercury does not garbage collect 
-	% the var, Lua will not garbage collect the refrenced variable.
+	% Find the top of the stack
 	%
-:- type lua.var.
+:- pred lua.top(state::in, int::out).
+:- func lua.top(state) = int.
 
-%-----------------------------------------------------------------------------%
-%
-% Passing values between Mercury and Lua 
-%	
-
-
-
-%-----------------------------------------------------------------------------%
-%
-% Lua objects/refrence types
-%
-
-% These types represent lua.vars of a specific Lua type.  In terms of Lua 
-% semantics, these variables are passed by refrence, not by value. 
-% 
-% This is important to keep in mind when performing equality tests.  
-% An equality test on two of these variables created seperately will fail, 
-% even if they both have the same literal value. Equality will succeed when
-% two refrence type variables refrence the same memory adress. 
-%
-% Note that the refrences discussed here are NOT normal C pointers, but values 
-% internal to Lua's register-based VM.  As a result, (with the exception of 
-% userdata) these values cannot be instantiated outside of Lua, only refrenced.
-%
-% Lua variables with assigned refrence types may be assigned metatables, 
-% Lua tables that store metadata about 
-
-	% In Lua, the only native data-structure is the table. The Lua table
-	% is implemented with a hybrid array/hash table data structure,
-	% allowing tables to be efficiently used in the place of arrays, lists
-	% maps and sets. 
-	%
-	% Tables may be assigned metatables, which can store metadata about
-	% 
-
-
-:- pred to_table(var::in, table::out) is semidet.
-:- func to_table(var) = table is semidet.
-
-:- pred push_table(state::in, table::in, var::out) is det.
-:- func push_table(state, table) = var is det.
-
-
-% Lua functions are impure, lexically scoped, may have a variable number of
-% arguments and return values, can be curried, can be passed freely as first
-% class variables.  For the sake of stability and sanity, Lua functions may
-% not be invoked directly from Mercury using this Library, only passed as
-% variables.
-
-:- pred to_function(var::in, function::out) is semidet.
-:- func to_function(var) = function is semidet.
-
-:- pred push_function(state::in, function::in, var::out) is det.
-:- func push_function(state, function) = var is det.
-
-
-	% coroutine passing
-
-:- pred to_thread(var::in, thread::out) is semidet.
-:- func to_thread(var) = thread is semidet.
-
-:- pred push_thread(state::in, thread::in, var::out) is det.
-:- func push_thread(state, thread) = var is det.
-
-
-	% userdata passing
-
-
-:- pred push_userdata(state::in, T::in, var::out) is det.
-:- func push_userdata(state, T) = var is det.
-
-:- pred push_userdata(state::in, table::in, T::in, var::out) is det.
-:- func push_userdata(state, table, T) = var is det.
-
-
-:- pred ref_state(ref::in, lua_state::out) is det.
-:- func ref_state(ref) = lua_state is det.
-
-:- pred var_equals(var::in, var::in) is semidet.
-
-%-----------------------------------------------------------------------------%
-
-
-	% In Lua, variables are not typed, values are.  Lua recognizes eight
-	% types.
-	%
-	% 'nil' represents the abscence of value. 
-	% 'number' is equivalent to float type.
-	% 'boolean' is equivalent to the bool type.
-	% 'string' is equivalent to the string type.
-	%
-	% 'table', a refrence to an associative array, used for lists and maps.
-	%	Unlike an assoc_list, a Lua table may not associate more than
-	%	one value with any given key, and thus, behaves more like the
-	% 	Mercury map type.
-	% 
-	% 'function' is a refrence to a Lua function, which are impure, 
-	%       may have a variable number of arguments, multiple return
-	%	values and can be passed freely like any other variable.
-	%
-	% 'userdata' is lua's type for handling foreign data, unless otherwise
-	%	noted, values stored as userdata are subject to collection by 	
-	% 	Lua's Garbage Collector.
-	%
-	% 'lightuserdata' is seen in Lua as identical to userdata, but contains
-	%	a C pointer which will not be collected by Lua's GC.
-	%
-	% 'thread' is a lua_state, usually a coroutine, note that the main
-	% lua_state should not be treated like a coroutine.
-	%
-:- type lua_type 
-	--->	none
-	;	nil
-	;	boolean
-	;	lightuserdata
-	;	number
-	;	string
-	;	table
-	;	function
-	;	userdata
-	;	thread.
-
-:- pred lua.type(var::in, lua_type::out) is det.
-:- func lua.type(var) = lua_type.
-
-%-----------------------------------------------------------------------------%
-%
-% Lua tables
-%
-
-
-% For the purposes of this library, values may only be assigned to unique 
-% tables created in Mercury.  Any tables that have been exposed to Lua
-% execution must be considered immutable to preserve Mercury's
-% pure declarative semantics.
-
-:- type lua.table.
-
-	% new_table(L, New_Table)
-	% Create a new, unique, empty table.
-	%
-:- pred new_table(state::in, table::uo) is det.
-:- func new_table(state) = table.
-:- mode new_table(in) = uo is det.
-
-	% new_table(L, Metatable, New_table)
-	% Create a new table and assign a metatable to it
-	%
-:- pred new_table(state::in, table::in, table::uo) is det.
-:- func new_table(state, table) = table.
-:- mode new_table(in, in) = uo is det.
-
-	% get(Table, Key, Value)
-	% Perform a raw lookup on a Lua table.
-	% Wil always return nil on lookups to nil keys.
-	%
-:- pred get(table, var, var).
-:- mode get(in, in, out) is det.
-:- mode get(in, out, out) is nondet.
-:- func get(table, var) = var is det.
-
-
-	% set(Table0, Table, Key, Value)
-	% Perform a raw destructive update on a unique table.
-	% Will ignore attempts to assign to nil key. 
-	%
-:- pred set(table::di, table::uo, var::in, var::in) is det.
-:- func set(table, var, var) = table.
-:- mode set(ui, in, in) = uo is det.
-
-%-----------------------------------------------------------------------------%
-	
-% Lua functions are either compiled chunks of Lua source code, or are C
-% function pointers as defined by the C type "lua_CFunction". Lua functions 
-% are varadic, accepting variable numbers of arguments and
-% return values.
-% 
-% Lua handles functions as first-class variables, they can be assigned
-% to variables and passed as function arguments or return values in
-% the same manner as any other value in Lua.
-% 
-% Lua functions are inherently impure.  Not only can they cause
-% side effects to variables passed as arguments, but they are lexically 
-% scoped, allowing side effects on any local variable declared in a scope
-% outside of the function's scope.
-%
-% This fact effectively removes any way of determining a Lua function's purity
-% outside of compiling a function directly with calls like loadstring and
-% dostring, or passing a lua_CFunction function pointer.
-
-	% A refrence to a 
-	% This type is identical to the var type, but with a garuntee that
-	% the value held by table is, in fact, a Lua table.
-	% 
-:- type lua.function.
-
-:- type lua.mr_function == (func(state) = list(var))
-
-	% Construct a Lua function from a higher order term.
-	%
-:- pred function(mr_function::in, function::out) is det.
-:- func function(mr_function) = function is det.
-
-	% A typedef for a C function pointer that may be passed to Lua as 
-	% a Lua function as defined in the Lua C API.
-	%
-:- type lua.c_function.
-
-	% Construct a Lua closure from a C function pointer.
-	%
-:- pred c_function(c_function::in, function::out) is det.
-:- func c_function(c_function) = function is det.
-
-
-
-	% Construct a Lua function from a C function pointer.
-	%
-:- pred c_function(c_function::in, function::out) is det.
-:- func c_function(c_function) = function is det.
-
-%-----------------------------------------------------------------------------%
-
-% TODO: coroutines?, iterators?, multi/nondet functions?
-
-%-----------------------------------------------------------------------------%
-%
-% Userdata
-%
-	% Lua handles all foreign values with the userdata value type.
-	% Under normal circumstances, Lua can't actually do anything with
-	% userdata. Attempts to use indexing, arithmetic or other operators
-	% will result in a lua error.  However, as with tables and functions,
-	% it is possible to assign metatables to userdata, allowing one to
-	% extend the valid syntax with which Lua can interact with a given
-	% userdata.
-	
-
-:- typeclass userdata(T) where [].
-
-
-/*  TODO Change metamethod names to avoid conflicts with Mercury
-:- type metamethod
-	--->	add	%	+
-	;	sub	%	-
-	;	mul	%	*
-	;	div	%	/
-	; 	mod	%	%
-	;	pow	%	^
-	;	unm	%	-
-	;	concat	%	..
-	;	len	%	#
-	;	eq	%	==
-	;	lt	%	<
-	;	le	%	<=
-	;	index	%	u[k]
-	;	newindex%	u[k] = v
-	;	call	%	u(...)
-	;	gc.
-*/
-
+:- pred lua.getstack(
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
