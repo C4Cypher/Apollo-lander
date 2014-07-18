61bdfe81431e84fa07a3acc6c93560d123a535ec
C4Cypher <c4cypher@gmail.com>
2014-07-18 12:51:44 -0400

    Better typeclass implementation.
    Needs testing!

--- impure_stack.m
+++ impure_stack.m
@@ -26,64 +26,88 @@
 % V refers to values on the stack
 
 	% Intended syntax: stack(S, int), X = (stack(Y), 1, 2, 3, 4, 5, 6)  
-	% ... this may not work.
-	% Intended to be shorthand
+	% !!!!!! this may not work.
+	% Intended to be shorthand for stack construction
+	%
 :- type stack(S, V)
-	---	stack(Stack)
-	;	stack(Stack, Value) , V.
+	---	stack(S)
+	;	stack(S, Value) , V.
 
-	% Polymorphic stack. 
+	% Polymorphic stack. !!!!!! Will this work?
 	%
 :- type stack(S) == stack(S, _).
 	
 :- type index == int.	
 	
 
+:- typeclass stack(S) where [
 
 	% Retreive the index at the top of the stack, (starting at 1 for the bottom).
 	% Fails if the stack is empty.
 	%
-:- func top(stack(S, V)) = index <= impure_stack(S, V) is semidet.
+	func top(S)
+	mode top(in) is semidet,
 
 	% Retreive the number of values on the stack.
 	% Effectively a det version of top.
 	%
-:- func count(stack(S, V)) = int <= impure_stack(S, V) is det.
-	
-	% retreive the value at the given index (starting at 1 for the bottom).
-	%
-:- func index(int, stack(S, V)) = V <= impure_stack(S, V) is semidet.
+	func count(S) = int
+	mode count(in) = out is det,
 	
 	% Succeed if the given index is valid.
 	%
-:- pred valid_index(index::in, stack(S, V)::in) <= impure_stack(S, V) is semidet.
+	pred valid_index(index, S),
+	mode valid_index(in, in) is semidet,
+	
+	% Pop a value off the stack.
+	%
+	func pop(S) = S,
+	mode pop(in) = out is det,
+	
+	% Pop a number of values off of the stack.
+	%
+	func pop(int, S) = S
+	mode pop(in, in) = out is det,
+	
+
+	% Pop values off the stack, failing if there aren't enough values to pop.
+	%
+	func safe_pop(int, S) = S is semidet,
+
+
+].
+
+:- typeclass stack(S, V) <= stack(S) where [
+
+	% retreive the value at the given index (starting at 1 for the bottom).
+	%
+	func index(index, S) = V
+	mode index(in, in) = out is semidet,
+	
+
 	
 	% Push a value onto the stack.
 	%
-:- func push(V, stack(S, V)) = stack(S, V) is det.
-
-	% Pop a number of values off of the stack.
+	func push(V, S) = S,
+	mode push(in, in) = out is det,
+	mode push(out, out) = in is det,
+	
+	% Push a list of values onto the stack.
 	%
-:- func pop(int, stack(S, V)) = stack(S, V) is det.
+	func push_list(list(V), S) = S,
 
-:- func safe_pop(int, stack(S, V)) = stack(S, V) is semidet.
-
-
+].
 
 
-:- typeclass impure_stack(S, V) where [
+:- typeclass impure_stack(S) where [
 
 	% retreive the index for the top value of the stack, fails if empty.
 	%
 	semipure func semipure_top(S) = int is semidet,
 	
-	% retreive the value at the given index (starting at 1 for the bottom).
+		% Check to see if a given index is valid.
 	%
-	semipure func semipure_index(int, S) = V is semidet,
-	
-	% Check to see if a given index is valid.
-	%
-	semipure pred semipure_valid(int, S) is semidet,
+	semipure pred semipure_valid(index, S) is semidet,
 	
 	% Ensure that it is safe to push a number of values onto the stack.
 	% Should fail the space for the new values cannot be allocated.
@@ -94,12 +118,7 @@
 	%
 	semipure pred stack_allocation_error(S::in) is erroneus,
 	
-	% Push a value onto the stack.
-	%
-	impure pred impure_push(S, V),
-	mode impure_push(in, in) is det,
-	
-	% Pop a number of values off the stack.
+		% Pop a number of values off the stack.
 	%
 	impure pop impure_pop(S, int),
 	mode impure_pop(in, in) is det,
@@ -108,14 +127,26 @@
 	%
 	semipure pred stack_pop_error(S::in) is erroneus, 
 	
-	% Make a copy of a stack
+
+].
+
+
+:- typeclass impure_stack(S, V) <= impure_stack(S) where [
+
+
+	% retreive the value at the given index (starting at 1 for the bottom).
 	%
-	semipure copy_stack(S) = S
+	semipure func semipure_index(index, S) = V is semidet,
+	
+	% Push a value onto the stack.
+	%
+	impure pred impure_push(S, V),
+	mode impure_push(in, in) is det,
+	
+	
 ].
 
 :- instance impure_stack(stack(S, V)) <= impure_stack(S, V).
-
-:- instance impure_stack(stack(S)) <= impure_stack(S, _).
 
 
 :- implementation.
