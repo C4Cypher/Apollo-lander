b12d4424c7e1c831063c6b54498928a4ef0f72c0
C4Cypher <c4cypher@gmail.com>
2014-07-18 20:07:49 -0400

    Most of the interface has been finished

--- pure_stack.m
+++ pure_stack.m
@@ -23,51 +23,149 @@
 :- import_module int.
 
 %-----------------------------------------------------------------------------%
-		
+%
+% Pure Stacks
+%
+
+% Normally, when Mercury comes into contact with mutable data structures;
+% usually when interacting with foreign code; Mercury either has to pretend
+% said structure is immutable, interact with it using an io__state, or handle
+% the onerous task of interacting with the data structure using impure calls.
+%
+% Stack data structures, (and other similar structures like cons lists) are an
+% interesting case for this scenario, as 
+
+
 	% Polymorphic type for handling impure stacks in a lazy, but pure
 	% manner.  The committed form caches the number of values to pop in
 	% order to return the stack to it's original form.
 	%
 :- type stack(T, V)
 	---> 	stack(T)
-	;	stack(stack(T, V), V),
-	;	impure_stack(stack(T,V), int).
+	;	stack(stack(T, V), V)
+	;	partial_stack(T, int)
+	;	impure_stack(pure_stack(T, V), int)
+	;	impure_stack(int). 	% For handling unique insts.
+
+%-----------------------------------------------------------------------------%
+%
+% Stack insts
+%
+
+	% This is the 'literal' representation of the enclosed value, without
+	% any queued changes to be made to 'commit' it.
+	%
+:- inst stack_root
+	---> 	stack(ground)
+	;	stack(unique)
+	;	stack(mostly_unique).
+
+	% Representation for a stack with queue'd values that will be pushed
+	% onto the stack once committed.
+:- inst lazy_stack
+	---> 	stack(stack_root, ground).
+
+	% This form of the stack 'pretends' that values have been popped off of
+	% it, to simplify equality testing. Without this, it would be akward
+	% comparing the stack with other stacks with less values.
+	%
+	% When this variation is 'committed' index values passed to the impure
+	% calls are ajusted accordingly.
+	%
+	% TODO: Work out the shuffling needed to pop values in the middle of a
+	% stack, or include a 'remove' call to do so in the impure_stack 
+	% typeclass.
+	%
+:- inst partial_stack
+	--->	stack(ground, ground)
+	;	stack(unique, ground)
+	;	stack(mostly_unique, ground).
 
 	% Inst for a stack constructed using pure semantics.
 	%
 :- inst pure_stack
-	--->	stack(ground)
+	--->	stack_root	
+	;	lazy_stack
+	;	partial_stack
 	;	stack(pure_stack, ground).
 
-	% This inst indicates that all of the stored values have been
-	% pushed onto the stack.
+
+	% This inst indicates that the mutable state of the stack has been
+	% modifed in some manner, and that the value is invalid for Mercury's
+	% declarative semantics until the changes are reverted.
 	%
-:- inst impure_stack ---> impure_stack(pure_stack).
+:- inst impure_stack 
+	---> 	comitted_stack
+	;	reduced_stack
+	;	impure_stack(ground).
+
+
+	% Impure stack that has values pushed onto it.
+	%
+:- inst comitted_stack
+	--->	impure_stack(pure_stack, ground).
+
+	% Impure stack that is unmodified, but any indexed calls to it will be
+	% shifted accordingly.
+	%
+:- inst reduced_stack
+	--->	impure_stack(partial_stack, bound(0)).
 
 
 	% Placing a stack in it's committed form within a pure stack's 
 	% recursive structure makes absolutely no sense, and has no 
-	% valid meaning.
+	% valid meaning.  The same applies for embedding a partial_stack
+	% within the middle of a pure_stack's structure.
 	%
 :- inst invalid_stack
 	---> 	stack(impure_stack, ground)
+	;	stack(stack(partial_stack, ground))
 	;	stack(invalid_stack, ground)
+	;	impure_stack(impure_stack).
 
+%-----------------------------------------------------------------------------%
+%
+% Stack modes
+%
+
+	% In and out modes for pure_stack.
+	%
+:- mode si == in(pure_stack).
+:- mode so == out(pure_stack).
+
+	% Mode for the moment a stack is readied for impure operations.
+	%
+:- mode commit == pure_stack >> impure_stack.
 
 	% Mode for the moment when a stack's values are committed to the stack.
 	%
-:- mode commit == pure_stack >> impure_stack
+:- mode commit_push == lazy_stack >> comitted_stack.
+
+
+	% Mode for the moment a partial stack is prepared for impure operations.
+	%
+:- mode commit_reduce == partial_stack >> reduced_stack.
 	
+	% Mode for the moment when a stack is returned to the state it was at
+	% when it was committed.
+	%
+:- mode revert == comitted_stack >> pure_stack.
+
 	% Mode for the moment when the values of a committed stack are popped
 	% off of the stack, returning it to it's pure form.
 	% 
-:- mode revert == impure_stack >> pure_stack.
+:- mode revert_pop == impure_stack >> lazy_stack.
+
+	% Reverting a reduced stack doesn't actually do much to the internal
+	% state. So this simply returns a reduced stack to it's partial form.
+	%
+:- mode revert_return == reduced_stack >> partial_stack.
 
 %-----------------------------------------------------------------------------%
 
 	% This form uses existential types to pass the values, making the
 	% stack polymorphic.  It's probably a better idea to use the more 
-	% general type.
+	% general type.  It's more of an experiment than anything.
 	%
 :- type stack(T) == stack(stack(T), stack_value(T)).	
 
@@ -82,56 +180,53 @@
 :- instance stack(stack(T)).
 :- instance stack(stack, stack_value).
 
+%-----------------------------------------------------------------------------%
+%
+%  The Pure stack typeclass
+%
 
+% TODO: Talk about this
 
-:- typeclass stack(S) where [
+:- typeclass pure_stack(T, V) <= impure_stack(T, V) where [
 
-	% Retreive the index at the top of the stack, (starting at 1 for the bottom).
+	% Retreive the int at the top of the stack, 
+	% starting at 1 for the bottom.
 	% Fails if the stack is empty.
 	%
-	func top(S)
+	func top(T)
 	mode top(in) is semidet,
 
 	% Retreive the number of values on the stack.
 	% Effectively a det version of top.
 	%
-	func count(S) = int
+	func count(T) = int
 	mode count(in) = out is det,
 	
-	% Succeed if the given index is valid.
+	% Succeed if the given int is valid.
 	%
-	pred valid_index(index, S),
+	pred valid_index(int, T),
 	mode valid_index(in, in) is semidet,
 	
 	% Pop a value off the stack.
 	%
-	func pop(S) = S,
+	func pop(T) = S,
 	mode pop(in) = out is det,
 	
 	% Pop a number of values off of the stack.
 	%
-	func pop(int, S) = S
+	func pop(int, T) = S
 	mode pop(in, in) = out is det,
-	
 
-	% Pop values off the stack, failing if there aren't enough values to pop.
+
+	% retreive the value at the given int (Ttarting at 1 for the bottom).
 	%
-	func safe_pop(int, S) = S is semidet,
-
-
-].
-
-:- typeclass stack(S, V) <= ((S -> V), stack(S)) where [
-
-	% retreive the value at the given index (starting at 1 for the bottom).
-	%
-	func index(index, S) = V
+	func index(int, T) = V
 	mode index(in, in) = out is semidet,
 	
 
 	% Push a value onto the stack.
 	%
-	func push(V, S) = S,
+	func push(V, T) = T,
 	mode push(in, in) = out is det,
 	mode push(out, out) = in is det,
 
@@ -140,55 +235,89 @@
 
 
 
-
 %-----------------------------------------------------------------------------%
+%
+%  Impure stack implementation
+%
 
-:- typeclass impure_stack(S) where [
+% TODO: Talk about this
 
-	% retreive the index for the top value of the stack, fails if empty.
+
+:- typeclass impure_stack(T) where [
+
+	% retreive the int for the top value of the stack, fails if empty.
 	%
-	semipure func semipure_top(S) = int is semidet,
+	semipure func semipure_top(T) = int is semidet,
 	
-		% Check to see if a given index is valid.
+		% Check to see if a given int is valid.
 	%
-	semipure pred semipure_valid(index, S) is semidet,
+	semipure pred semipure_valid(int, T) is semidet,
 	
 	% Ensure that it is safe to push a number of values onto the stack.
 	% Should fail the space for the new values cannot be allocated.
 	%
-	impure pred impure_free(S::in, int::in) is semidet,
+	impure pred impure_free(T::in, int::in) is semidet,
 	
 	% The error to call when impure_free fails.
 	%
-	semipure pred stack_allocation_error(S::in) is erroneus,
+	semipure pred stack_allocation_error(T::in) is erroneus,
 	
 		% Pop a number of values off the stack.
 	%
-	impure pop impure_pop(S, int),
+	impure pop impure_pop(T, int),
 	mode impure_pop(in, in) is det,
 	
 	% The error to call when one tries to pop off more values than exist.
 	%
-	semipure pred stack_pop_error(S::in) is erroneus, 
+	semipure pred stack_pop_error(T::in) is erroneus, 
 	
 
 ].
 
 
-:- typeclass impure_stack(S, V) <= impure_stack(S) where [
+:- typeclass impure_stack(T, V) <= impure_stack(T) where [
 
 
-	% retreive the value at the given index (starting at 1 for the bottom).
+	% retreive the value at the given int (Ttarting at 1 for the bottom).
 	%
-	semipure func semipure_index(index, S) = V is semidet,
+	semipure func semipure_index(int, T) = V is semidet,
 	
 	% Push a value onto the stack.
 	%
-	impure pred impure_push(S, V),
+	impure pred impure_push(T, V),
 	mode impure_push(in, in) is det,
 	
 	
 ].
+
+%-----------------------------------------------------------------------------%
+%
+% Polymorphic impure stacks
+%
+
+	% A polymorphic form of impure_stack, using a higher level type
+	% to store the more specific type polymorphically.
+	%
+:- typeclass impure_stack(T, U, V) <= (
+		(T -> U), (V -> U), 
+		pure_stack(T, V), 
+		pure_stack(T, U)
+) where [
+	
+	% Cast the more specific type to the more general type.
+	%
+	func upcast(V) = U,
+	mode upcast(in) = out is det,
+	
+	% Cast the more general type to the more specific type, fail if
+	% V does not match the type of the value stored by U
+	func downcast(U) = V,
+	mode downcast(in) = out is semidet
+
+].
+
+%-----------------------------------------------------------------------------%
+
 
 
 
