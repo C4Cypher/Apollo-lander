a0c4e74814954d6e8a8b15b35a40fe43e3f26e9c
C4Cypher <c4cypher@gmail.com>
2014-07-24 10:13:42 -0400

    Create lua.module.m

---
+++ lua.module.m
@@ -0,0 +1,19 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2014 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: module.m.
+% Main author: C4Cypher.
+% Stability: low.
+% 
+% This module presents types and predicates for constructing Lua loadable 
+% modules.
+%
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- module lua.module.
