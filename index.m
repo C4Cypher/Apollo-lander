3941184bcbacf7e0633b78ceb3799d2dc6730278
C4Cypher <c4cypher@gmail.com>
2014-08-08 23:17:36 -0400

    Redefined index to only govern int values.

--- index.m
+++ index.m
@@ -10,7 +10,7 @@
 % Main author: C4Cypher.
 % Stability: low.
 % 
-% TODO
+% An abstract typeclass interface for collections indexed but numeric values. 
 %
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -21,38 +21,132 @@
 
 :- import_module int.
 
-:- typeclass index(T, Key) where [
-	pred index(T, Key, Value),
-	mode index(in, in, out) is semidet,
-	mode index(in, unused, out) is nondet,
-	mode index(in, out, out) is nondet
+%-----------------------------------------------------------------------------%
+%
+% Indexes
+%
+	% index(Id, Value, Index):
+	%
+	% Represents an Index that uniquely associates Key to value in a one
+	% to one relationship. Value should be treated as existential.
+	%
+:- typeclass index(T) where [
+	pred index(int, V, T),
+	mode index(in, out, in) is semidet,
+	mode index(out, out, in) is nondet
 ].
 
-:- typeclass new_index(T, Key) <= index(T, Key) where [
-	pred new_index(Index, T, Key, Value) <= index(Index, Key),
-	mode new_index(in, out, in, in) is det
+	% Index ^ index(Id) = Value
+	%
+:- func index(int, T) = V is semidet <= index(T).
+
+	% Provide the valid numeric indexes of T.
+	%
+:- pred index(int::out, T) is nondet. 
+
+	% bounds(Count, First, Last, Index):
+	%
+	% Count the number of values in an index, providing the first and last
+	% indexes.
+	%
+:- pred bounds(int::out, int::out, int::out, T::in) is det. 
+
+	% Count the number of values in an index.
+	%
+:- func count(T) = int <= index(T).
+
+	% The lowest index
+	%
+:- func first(T) = int <= index(T).
+
+	% The highest index
+	%
+:- func last(T) = int <= index(T).
+
+	% Validate that there are no unbound values between the first and the
+	% last values.
+	%
+:- pred linear(T) is semidet <= index(T).
+	
+	% linear(!Bounds, Index):
+	%
+	% Validate that the given index has no unbound values in the given 
+	% Bounds.
+	%
+:- pred linear(int::in, int::in, T::in) is semidet <= index(T).
+
+
+%-----------------------------------------------------------------------------%
+%
+% Modifying Indexes
+%
+	
+	% new_index(Key, Value, Old, New):
+	%
+	% Create a new index by assigning a key to a value.
+	%
+:- typeclass new_index(T) <= index(T) where [
+	pred new_index(int, V, T0, T) <= index(T0),
+	mode new_index(in, in, in, out) is det
 ].
 
-:- func T ^ index(Key) = Value is semidet <= index(T, Key).
+	% (Old ^ index(Key) := Value) = New
+	%
+:- func 'index :='(Key, T0, Value) = T 
+	<= (index(T0, Key), new_index(T, Key)).
 
-:- func (In ^ index(Key) := Value) = Out 
-	<= (index(In, Key), new_index(Out, Key)).
+	% Add a value after the last index.
+	%	
+:- pred push(V::in, T0::in, T::out) is det <= (index(T0),new_index(T)).
+:- func push(V, T0) = T <= (index(T0),new_index(T)).
 
-:- func length(T) = int <= index(T,_).
-:- pred length(T::in, int::out) is det <= index(T, _).
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
 
 :- implementation.
 
 :- import_module solutions.
 
-Index ^ index(Key) = Value :- index(Index, Key, Value).
+Index ^ index(Id) = Value :- index(Id, Value, Index).
 
-In ^ index(Key) := Value = Out :- new_index(In, Out, Key, Value).
+:- index(I, T) :- index(I, _, T).
 
-length(T) = L :- 
-	aggregate(index(T,_), (func(_, N) = N + 1), 0, L).
+bounds(C, F, L, T) :-
+	( A = func(I, U@{Count, First0, Last0}) = {Count + 1, First, Last} :-
+		( U = {0, 0, 0} -> 
+			First = I, Last = I
+		; 
+			First = (First0 < I -> First0 ; I),
+			Last = (Last0 > I -> Last0 ; I)
+		)
+	),
+	(P = pred(I) :- index(I, T)),
+	aggregate(P, A, {0, 0, 0}, {C, F, L}).
 
-length(T, length(T)).
+count(T) = C :- bounds(C, _, _, T).
+first(T) = F :- bounds(_, F, _, T).
+last(T) = L :- bounds(_, _, L, T).
+
+linear(T) :- 
+	bounds(_, F, L, T),	
+	linear(F, L, T).
+	
+linear(F, L, T) :-
+	( index(F@L, T)
+	; 
+		F < L,
+		index(F, T),
+		linear(F + 1, L, T)
+	). 
+	
+
+%-----------------------------------------------------------------------------%
+
+
+In ^ index(Id) := Value = Out :- new_index(In, Out, Id, Value).
+
+
 
 		
 		
