572cbcdd9d6362b5a1dafcc0d7be6e8794b823b6
C4Cypher <c4cypher@gmail.com>
2014-08-15 09:24:11 -0400

    Template for the .m files.

---
+++ template.m
@@ -0,0 +1,20 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2014 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: filename.m.
+% Main author: c4cypher.
+% Stability: low.
+% 
+% Describe the module.
+% 
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- module template.
+
+
